ba051513ec3f0ef6a5577af872e870f2b1388f7b
Tanner Holle <tannerholle96@gmail.com>
2021-12-02 11:59:51 -0700

    getting closer to hosting the application

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -4,12 +4,21 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/EasyDraw', {useNewUrlParser: true}).then(() =>{
-  console.log("connected to MongoDB successfully :)")
-}).catch((e) => {
-  console.log("Error while trying to connect to MongoDB")
-  console.log(e)
-});
+/** If we are connecting to mongo Atlas */
+mongoose.connect('mongodb+srv://admin:EasyDrawAdmin@easydraw.kh14q.mongodb.net/EasyDraw?retryWrites=true&w=majority').then(() =>{
+    console.log("connected to MongoDB successfully :)")
+  }).catch((e) => {
+    console.log("Error while trying to connect to MongoDB")
+    console.log(e);
+  });
+
+/** If we are connecting to a mongo db on the local machine */
+// mongoose.connect('mongodb://localhost:27017/EasyDraw', {useNewUrlParser: true}).then(() =>{
+//   console.log("connected to MongoDB successfully :)")
+// }).catch((e) => {
+//   console.log("Error while trying to connect to MongoDB")
+//   console.log(e)
+// });
 
 // mongoose.set('useCreateIndex', true);
 // mongoose.set('useFindAndModify', false);
