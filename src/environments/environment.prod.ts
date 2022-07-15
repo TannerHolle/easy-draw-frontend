c6bf99cb05785928147f7607a7a92221c8969272
Tanner Holle <tannerholle96@gmail.com>
2022-07-14 20:56:11 -0600

    updated server url to dev server for dev environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://easydrawapiprod.eba-kwhqpdw4.us-west-1.elasticbeanstalk.com/api'
+  apiUrl: 'http://easydrawapidev.eba-kwhqpdw4.us-west-1.elasticbeanstalk.com/api'
 };
