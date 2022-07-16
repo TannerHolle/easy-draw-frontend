9b78db6598d41465d902d5452906e3575e2bf763
Tanner Holle <tannerholle96@gmail.com>
2022-07-16 11:14:17 -0600

    point at the right server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'http://easydrawapidev.eba-kwhqpdw4.us-west-1.elasticbeanstalk.com/api'
 };
 
 /*
