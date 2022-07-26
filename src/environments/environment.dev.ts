a10e8e4d16fad903a4d3600e3aaf1a995f812754
Tanner Holle <tannerholle96@gmail.com>
2022-07-26 09:00:41 -0600

    made the backend https

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 //Server URL for dev
 export const environment = {
   production: false,
-  apiUrl: 'http://easydrawapidev.eba-kwhqpdw4.us-west-1.elasticbeanstalk.com/api'
+  apiUrl: 'https://easydrawapidev.eba-kwhqpdw4.us-west-1.elasticbeanstalk.com/api'
 };
