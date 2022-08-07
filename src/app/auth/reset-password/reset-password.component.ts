e591af169eb379fcb1f41cdd6262f7d5cfd1d043
MuzamilBukhari <muzamil.bukhari.39@gmail.com>
2022-08-07 18:21:08 +0500

    added password validation in reset password

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -16,6 +16,12 @@ export class ResetPasswordComponent implements OnInit {
   secretId;
   message = 'Please wait while we verify link!';
   email;
+  passwordValidations = {
+    uppercaseLetter: false,
+    lowercaseLetter: false,
+    aNumber: false,
+    minimumEightChars: false
+  }
 
   constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
@@ -36,6 +42,42 @@ export class ResetPasswordComponent implements OnInit {
     });
   }
 
+  onPasswordChange(value) {
+    const lowerCaseLetters = /[a-z]/g;
+    if (value.match(lowerCaseLetters)) {
+      this.passwordValidations.lowercaseLetter = true;
+    } else {
+      this.form.controls['password'].setErrors({ 'incorrect': true });
+      this.passwordValidations.lowercaseLetter = false;
+    }
+
+    // Validate capital letters
+    const upperCaseLetters = /[A-Z]/g;
+    if (value.match(upperCaseLetters)) {
+      this.passwordValidations.uppercaseLetter = true;
+    } else {
+      this.form.controls['password'].setErrors({ 'incorrect': true });
+      this.passwordValidations.uppercaseLetter = false;
+    }
+
+    // Validate numbers
+    const numbers = /[0-9]/g;
+    if (value.match(numbers)) {
+      this.passwordValidations.aNumber = true;
+    } else {
+      this.form.controls['password'].setErrors({ 'incorrect': true });
+      this.passwordValidations.aNumber = false;
+    }
+
+    // Validate length
+    if (value.length >= 8) {
+      this.passwordValidations.minimumEightChars = true;
+    } else {
+      this.form.controls['password'].setErrors({ 'incorrect': true });
+      this.passwordValidations.minimumEightChars = false;
+    }
+  }
+
   verifyLink(id) {
     this.http.post(environment.apiUrl + '/user/verify-reset-password-link', { id })
       .subscribe((response: any) => {
@@ -54,6 +96,9 @@ export class ResetPasswordComponent implements OnInit {
   }
 
   updatePassword() {
+    if (this.form.invalid) {
+      return;
+    }
     if (this.email == undefined) {
       alert('Something went wrong')
       return false;
